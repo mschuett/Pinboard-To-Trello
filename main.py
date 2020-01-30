533f7d85cf6e3083ef9eb1bee26ff6fb8b25e974
Martin Schütte <info@mschuette.name>
2020-01-30 22:00:23 +0100

    black formatting

--- main.py
+++ main.py
@@ -9,87 +9,101 @@ from pinboard import pinboard
 from trello import TrelloClient
 
 
-CONFIG_FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json')
-LOG_FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log.txt')
-AUTH_DATA_KEY = 'authentication'
+CONFIG_FILE_NAME = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "config.json"
+)
+LOG_FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log.txt")
+AUTH_DATA_KEY = "authentication"
 
 
 # Enable logging
-file_handler = RotatingFileHandler(
-    LOG_FILE_NAME, maxBytes=(1048576 * 5), backupCount=7
-)
+file_handler = RotatingFileHandler(LOG_FILE_NAME, maxBytes=(1048576 * 5), backupCount=7)
 console_handler = logging.StreamHandler()
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    handlers=[file_handler, console_handler])
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    handlers=[file_handler, console_handler],
+)
 logger = logging.getLogger(__name__)
 
 # Reading configuration and authentication data
-with open(CONFIG_FILE_NAME, 'r') as conf_file:
+with open(CONFIG_FILE_NAME, "r") as conf_file:
     conf_data = json.load(conf_file)
 auth_data = conf_data[AUTH_DATA_KEY]
 
 # Logging in to Pinboard
-pb = pinboard.Pinboard(conf_data[AUTH_DATA_KEY]['pinboard_api_token'])
-logger.info('Logged in to Pinboard')
+pb = pinboard.Pinboard(conf_data[AUTH_DATA_KEY]["pinboard_api_token"])
+logger.info("Logged in to Pinboard")
 
 # Logging in to Trello
 trello_client = TrelloClient(
-    api_key=conf_data[AUTH_DATA_KEY]['trello_api_key'],
-    token=conf_data[AUTH_DATA_KEY]['trello_token']
+    api_key=conf_data[AUTH_DATA_KEY]["trello_api_key"],
+    token=conf_data[AUTH_DATA_KEY]["trello_token"],
 )
-logger.info('Logged in to Trello')
-trello_list = trello_client.get_list(conf_data['trello_list_id'])
+logger.info("Logged in to Trello")
+trello_list = trello_client.get_list(conf_data["trello_list_id"])
 trello_board = trello_list.board
 trello_labels = {}
 for label in trello_board.get_labels():
     label.fetch()
     trello_labels[label.name] = label
-logging.debug('Labels: %s', trello_labels)
+logging.debug("Labels: %s", trello_labels)
 
-trello_label_color_generator = itertools.cycle([
-    "green", "yellow", "orange", "red", "purple",
-    "blue", "sky", "lime", "pink", "black"])
+trello_label_color_generator = itertools.cycle(
+    [
+        "green",
+        "yellow",
+        "orange",
+        "red",
+        "purple",
+        "blue",
+        "sky",
+        "lime",
+        "pink",
+        "black",
+    ]
+)
 
-pb_tag = conf_data['pinboard_tag']
+pb_tag = conf_data["pinboard_tag"]
 new_pb_items = pb.posts.all(tag=[pb_tag])
-logger.info('Fetched new Pinboard items')
+logger.info("Fetched new Pinboard items")
 
 if len(new_pb_items) == 0:
-    logger.info('No new items.')
+    logger.info("No new items.")
     sys.exit(0)
 
 for pb_item in new_pb_items:
-        logger.debug(f"""Found item with
+    logger.debug(
+        f"""Found item with
             url: {pb_item.url},
             description: {pb_item.description},
             extended: {pb_item.extended},
             tags: {pb_item.tags},
             time: {pb_item.time},
-            """)
+            """
+    )
 
-        card = trello_list.add_card(name=pb_item.description,
-                                    desc=pb_item.extended)
-        logger.info(f'Created card \'{pb_item.description}\')')
+    card = trello_list.add_card(name=pb_item.description, desc=pb_item.extended)
+    logger.info(f"Created card '{pb_item.description}')")
 
-        card.attach(url=pb_item.url)
-        logger.info(f'Attached link {pb_item.url} to item')
+    card.attach(url=pb_item.url)
+    logger.info(f"Attached link {pb_item.url} to item")
 
-        for tag in pb_item.tags:
-            # ignore marker tag
-            if pb_tag and tag == pb_tag:
-                continue
-            # create new label
-            if tag not in trello_labels:
-                label_color = next(trello_label_color_generator)
-                label = trello_board.add_label(tag, label_color)
-                trello_labels[tag] = label
-            card.add_label(trello_labels[tag])
+    for tag in pb_item.tags:
+        # ignore marker tag
+        if pb_tag and tag == pb_tag:
+            continue
+        # create new label
+        if tag not in trello_labels:
+            label_color = next(trello_label_color_generator)
+            label = trello_board.add_label(tag, label_color)
+            trello_labels[tag] = label
+        card.add_label(trello_labels[tag])
 
-        pb.posts.delete(url=pb_item.url)
-        logger.info('Deleted Pinboard item %s', pb_item.url)
+    pb.posts.delete(url=pb_item.url)
+    logger.info("Deleted Pinboard item %s", pb_item.url)
 
-conf_data['pinboard_last_checked'] = now_timestamp
-with open(CONFIG_FILE_NAME, 'w') as conf_file:
+conf_data["pinboard_last_checked"] = now_timestamp
+with open(CONFIG_FILE_NAME, "w") as conf_file:
     json.dump(conf_data, conf_file, indent=2)
